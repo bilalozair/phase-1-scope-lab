db56265bfd44d6a271c7f808d06a2d33057a7d9f
bilalozair <bo569@nyu.edu>
2022-03-05 03:28:01 +0500

    commit tree c27944322f21dd84f3d9b244fd11e2bc1a47179e

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'maybe bob';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bilal';
+    return leastFavoriteCustomer;
+}
